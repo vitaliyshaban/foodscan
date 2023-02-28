06a206b682a7b5774240111301bbe25621a32678
vitaliyshaban <vitaliyshaban@gmail.com>
2023-02-28 16:35:07 +0400

    update

---
+++ src/components/header/index.tsx
@@ -0,0 +1,11 @@
+import './styles.scss'
+
+function Header() {
+	return (
+		<header className="App-header">
+			<div className="logo">Foodscan</div>
+		</header>
+	);
+}
+
+export default Header;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,58 @@
-import React from 'react';
-import './styles.scss';
+import { useState, useRef } from "react";
+import "./styles.scss";
+
+import Header from "../components/header";
+import Scanner from "../components/scanner";
+import Result from "../components/scanner/result";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        heder
-      </header>
-    </div>
-  );
+	const [scanning, setScanning] = useState<any>(false);
+	const [results, setResults] = useState<any>([]);
+	const scannerRef = useRef<any>(null);
+    console.log(results)
+	return (
+		<div className="App">
+			<Header />
+
+			<div>
+				<button onClick={() => setScanning(!scanning)}>
+					{scanning ? "Stop" : "Start"}
+				</button>
+				<ul className="results">
+					{results.map(
+						(result:any) =>
+							result.codeResult && (
+								<Result key={result.codeResult.code} result={result} />
+							)
+					)}
+				</ul>
+				<div
+					ref={scannerRef}
+					style={{ position: "relative", border: "3px solid red" }}>
+					{/* <video style={{ width: window.innerWidth, height: 480, border: '3px solid orange' }}/> */}
+					<canvas
+						className="drawingBuffer"
+						style={{
+							position: "absolute",
+							top: "0px",
+							// left: '0px',
+							// height: '100%',
+							// width: '100%',
+							border: "3px solid green",
+						}}
+						width="640"
+						height="480"
+					/>
+					{scanning ? (
+						<Scanner
+							scannerRef={scannerRef}
+							onDetected={(result:any) => setResults([...results, result])}
+						/>
+					) : null}
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
