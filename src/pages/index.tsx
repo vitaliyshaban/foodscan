b3d3fff931093f500194ac8b7fc1335cbe223ae5
vitaliyshaban <vitaliyshaban@gmail.com>
2023-02-28 14:20:50 +0400

    add

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        heder
+      </header>
+    </div>
+  );
+}
+
+export default App;
