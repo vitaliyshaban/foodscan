21389d54d4a1de341d451ba71e744e1bb6840a4f
vitaliyshaban <vitaliyshaban@gmail.com>
2023-03-01 17:22:02 +0400

    qwe

--- src/components/scanner/index.tsx
+++ src/components/scanner/index.tsx
@@ -1,48 +1,54 @@
-import { useEffect, useLayoutEffect, useState } from "react";
-import Notifications from "../notifications"
+import { Html5QrcodeScanner, Html5Qrcode } from "html5-qrcode";
+import { useEffect, useState } from "react";
 
-function Scaner({Html5QrcodeScanner}:any) {
-    const [barcode, setBarcode] = useState<any>(null);
+import Notifications from "../notifications";
+
+const qrcodeRegionId = "html5qr-code-full-region";
+
+const Scanner = () => {
+	const [barcode, setBarcode] = useState<any>(null);
+	const [camera, setCamera] = useState<any>();
+
 	useEffect(() => {
-	    // return () => {
-	        if (Html5QrcodeScanner) {
-	            let html5QrcodeScanner = new Html5QrcodeScanner(
-	                "reader",
-	                { facingMode: "user", fps: 100, qrbox: { width: 320, height: 200 } },
-	                false
-	            );
-	            html5QrcodeScanner.render(
-	                (data: any) => {
-                        setBarcode(data)
-                    },
-	                // (err: any) => console.log("err ->", err)
-	            );
-	        }
-            // };
-	}, [Html5QrcodeScanner]);
+		const html5QrCode = new Html5Qrcode(qrcodeRegionId);
+		Html5Qrcode.getCameras()
+			.then((devices) => {
+				if (devices) {
+					setCamera(devices[0].id);
+				}
+			})
+			.catch((err) => {});
+		// return () => {
+		if (camera) {
+			console.log(camera);
+			html5QrCode
+				.start(
+					camera,
+					{
+						fps: 10, // Optional, frame per seconds for qr code scanning
+						qrbox: { width: 250, height: 250 }, // Optional, if you want bounded box UI
+					},
+					(decodedText, decodedResult) => {
+						setBarcode(decodedText);
+						console.log(decodedText);
+					},
+					(errorMessage) => {
+						// parse error, ignore it.
+					}
+				)
+				.catch((err) => {
+					// Start failed, handle it.
+				});
+		}
+		// };
+	}, [camera]);
 
-	// beware: id must be the same as the first argument of Html5QrcodeScanner
 	return (
 		<>
-			<div id="reader"></div>
-            <Notifications barcode={barcode} satate={barcode ? true : false} />
-			{/* {error && `Devices error: ${error}`}
-			{devices && (
-				<div>
-					<span>Available devices are:</span>
-					<ul>
-						{devices.map((v) => (
-							<li>
-								id: {v.id}
-								<br />
-								label: {v.label}
-							</li>
-						))}
-					</ul>
-				</div>
-			)} */}
+			<div id={qrcodeRegionId} />
+			<Notifications barcode={barcode} satate={barcode ? true : false} />
 		</>
 	);
-}
+};
 
-export default Scaner;
+export default Scanner;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,15 @@
 import "./styles.scss";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Header from "../components/header";
 import Scanner from "../components/scanner";
-// import Result from "../components/scanner/result";
-import {
-	useHtml5QrCodeScanner,
-	useAvailableDevices,
-} from "react-html5-qrcode-reader";
-
-const html5QrCodeScannerFile = process.env.PUBLIC_URL + "/html5-qrcode.min.js";
 
 function App() {
 	const [openScanner, setOpenScanner] = useState<boolean>(false);
-	const { Html5QrcodeScanner } = useHtml5QrCodeScanner(html5QrCodeScannerFile);
-	//const { devices, error } = useAvailableDevices(html5QrCodeScannerFile);
-
 	return (
 		<div className="App">
 			<Header />
 			{openScanner ? (
-				<Scanner Html5QrcodeScanner={Html5QrcodeScanner} />
+				<Scanner />
 			) : (
 				<div onClick={() => setOpenScanner(true)}>Open</div>
 			)}
