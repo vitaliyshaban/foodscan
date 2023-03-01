aca6865d26d8a7178c8c1a6444962f7c2c31640f
vitaliyshaban <vitaliyshaban@gmail.com>
2023-03-01 17:48:53 +0400

    Update index.tsx

--- src/components/scanner/index.tsx
+++ src/components/scanner/index.tsx
@@ -7,45 +7,55 @@ const qrcodeRegionId = "html5qr-code-full-region";
 
 const Scanner = () => {
 	const [barcode, setBarcode] = useState<any>(null);
-	const [camera, setCamera] = useState<any>();
+	// const [devices, setDevices] = useState<any>();
+	// const [camera, setCamera] = useState<any>();
 
 	useEffect(() => {
+        return () => {
 		const html5QrCode = new Html5Qrcode(qrcodeRegionId);
 		Html5Qrcode.getCameras()
 			.then((devices) => {
 				if (devices) {
-					setCamera(devices[0].id);
+					// console.log(devices);
+					// setCamera(devices);
+                    let device = devices[0].id;
+                    if(devices && devices.length > 1) {
+                        device = devices[1].id;
+                    }
+					html5QrCode
+						.start(
+							// { deviceId: { exact: devices[0].id } },
+							device,
+							{
+								fps: 10, // Optional, frame per seconds for qr code scanning
+								qrbox: { width: 250, height: 250 }, // Optional, if you want bounded box UI
+							},
+							(decodedText, decodedResult) => {
+								setBarcode(decodedText);
+								// console.log(decodedText);
+							},
+							(errorMessage) => {
+								// console.log(errorMessage);
+							}
+						)
+						.catch((err) => {
+							// Start failed, handle it.
+						});
 				}
 			})
 			.catch((err) => {});
+        }
 		// return () => {
-		if (camera) {
-			console.log(camera);
-			html5QrCode
-				.start(
-					{ facingMode: "environment" },
-					{
-						fps: 10, // Optional, frame per seconds for qr code scanning
-						qrbox: { width: 250, height: 250 }, // Optional, if you want bounded box UI
-					},
-					(decodedText, decodedResult) => {
-						setBarcode(decodedText);
-						console.log(decodedText);
-					},
-					(errorMessage) => {
-						// parse error, ignore it.
-					}
-				)
-				.catch((err) => {
-					// Start failed, handle it.
-				});
-		}
+		// 	if (camera) {
+		// 		console.log(camera);
+		// 	}
 		// };
-	}, [camera]);
+	}, []);
 
 	return (
 		<>
 			<div id={qrcodeRegionId} />
+			<div></div>
 			<Notifications barcode={barcode} satate={barcode ? true : false} />
 		</>
 	);
