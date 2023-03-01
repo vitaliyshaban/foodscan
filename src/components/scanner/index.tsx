1d5377ac28bb001e32d5787a0a738dee54a035d6
vitaliyshaban <vitaliyshaban@gmail.com>
2023-03-01 18:24:37 +0400

    Update index.tsx

--- src/components/scanner/index.tsx
+++ src/components/scanner/index.tsx
@@ -4,57 +4,60 @@ import { useEffect, useState } from "react";
 import Notifications from "../notifications";
 
 const qrcodeRegionId = "html5qr-code-full-region";
-let html5QrCode:any;
+let html5QrCode: any;
 
 const Scanner = () => {
-	const [barcode, setBarcode] = useState<any>(null);
+	const [barcode, setBarcode] = useState<string>("");
 	// const [initCam, setInitCam] = useState<boolean>(false);
 	// const [camera, setCamera] = useState<any>();
 	// console.log(initCam)
 	useEffect(() => {
 		// return () => {
 		html5QrCode = new Html5Qrcode(qrcodeRegionId);
-		Html5Qrcode.getCameras()
-			.then((devices) => {
-				if (devices) {
-					let device = devices[0].id;
-					if (devices && devices.length > 1) {
-						device = devices[1].id;
-					}
-					console.log(device);
-					html5QrCode
-						.start(
-							// { deviceId: { exact: devices[0].id } },
-							device,
-							{
-								fps: 10, // Optional, frame per seconds for qr code scanning
-								qrbox: { width: 340, height: 250 }, // Optional, if you want bounded box UI
-							},
-							(decodedText:any, decodedResult:any) => {
-								setBarcode(decodedText);
-								// console.log(decodedText);
-							},
-							(errorMessage:any) => {
-								// console.log(errorMessage);
-							}
-						)
-						.then(() => {
-							console.log("init");
-						})
-						.catch((err:any) => {
-							// Start failed, handle it.
-						});
+		html5QrCode
+			.start(
+				// { deviceId: { exact: devices[0].id } },
+				// device,
+				{ facingMode: "user" },
+				{
+					fps: 10, // Optional, frame per seconds for qr code scanning
+					qrbox: { width: 340, height: 250 }, // Optional, if you want bounded box UI
+				},
+				(decodedText: any, decodedResult: any) => {
+					setBarcode(decodedText);
+					console.log(decodedText);
+					console.log(decodedResult);
+				},
+				(errorMessage: any) => {
+					// console.log(errorMessage);
 				}
+			)
+			.then(() => {
+				console.log("init");
 			})
-			.catch((err) => {});
+			.catch((err: any) => {
+				// Start failed, handle it.
+			});
+		// Html5Qrcode.getCameras()
+		// 	.then((devices) => {
+		// 		if (devices) {
+		// 			let device = devices[0].id;
+		// 			if (devices && devices.length > 1) {
+		// 				device = devices[1].id;
+		// 			}
+		// 			console.log(device);
+
+		// 		}
+		// 	})
+		// 	.catch((err) => {});
 	}, []);
 	const stopScan = () => {
 		html5QrCode
 			.stop()
-			.then((ignore:any) => {
-				console.log(ignore)
+			.then((ignore: any) => {
+				console.log(ignore);
 			})
-			.catch((err:any) => {
+			.catch((err: any) => {
 				// Stop failed, handle it.
 			});
 	};
